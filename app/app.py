93f16dd845118b456345db5a56236eedc4e81e17
Nagpal45 <nagpal4503@gmail.com>
2024-09-22 09:22:44 +0530

    utils & functions

--- app/app.py
+++ app/app.py
@@ -1,4 +1,36 @@
 from flask import Flask, render_template, request, Markup
+import numpy as np
+import pandas as pd
+from utils.fertilizer import fertilizer_dic
+import requests
+import config
+import pickle
+
+crop_recommendation_model_path = 'models/RandomForest.pkl'
+crop_recommendation_model = pickle.load(
+    open(crop_recommendation_model_path, 'rb'))
+
+def weather_fetch(city_name):
+    """
+    Fetch and returns the temperature and humidity of a city
+    :params: city_name
+    :return: temperature, humidity
+    """
+    api_key = config.weather_api_key
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
 
 app = Flask(__name__)
 
